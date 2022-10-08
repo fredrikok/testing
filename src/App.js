71150617dec1613a886545504193e68cdc9257ff
Fredrik Olsen Kapoun <83677247+fredrikok@users.noreply.github.com>
2022-10-08 15:36:07 +0200

    Testing 😭😭😭😭

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Fragment, useState } from "react";
 import './App.css';
+import Question from './data/questions.json'
+
+  const Quiz = Question
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [curQuestion, setCurQuestion] = useState(0)
+  const [points, setPoints] = useState(0)
+  const [isShown, setIsShown] = useState(false);
+
+  if (points === -5) {
+    alert("Skjerp deg!!!!")
+  }
+  if (curQuestion === 4) {
+    console.log("u won")
+  }
+//Logikk XD
+  const checkAnswer = (correct) => {
+    if (correct) {
+      console.log("Correct!")
+      setCurQuestion(curQuestion +1);
+      setPoints(points + 1);
+
+      setIsShown(current => false);
+
+
+    } else {
+      console.log("Wrong!")
+      setPoints(points - 1);
+      setIsShown(current => true);
+  }
 }
 
+
+  return (
+    <>
+    <span>Question {curQuestion + 1}</span>/{Quiz.length}     
+    <br></br>     
+    <span>Points : {points}</span>
+        <div className="App">
+          <h1>{Quiz[curQuestion].title}</h1>
+ 
+          <div className='answer'>
+						{Quiz[curQuestion].answers.map((answere) => (
+							<button onClick={() => checkAnswer(answere.correct)}>{answere.answere}</button>
+						))}
+					</div>
+
+          <div id="helper" className="helper" style={{display: isShown ? 'block' : 'none'}}>
+            <p>Hjelpe tekst : </p>
+            <p>{Quiz[curQuestion].helper.text}</p> 
+            <p>😎😎😎😎😎😎😎😎😎</p>           
+          </div>
+        </div>
+        </>
+      );
+    }
 export default App;
+
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+import { checkAnswer } from './App';
+
+
+it('Sould have 4 different h1 values that changes when you click the right answer', () => {
+  render(<App />)
+  const quizTitle = document.querySelector('h1')
+  expect(quizTitle).toHaveTextContent('Hvor mange timer er det i en dag?')
+  fireEvent.click(screen.getByText('24'))
+  expect(quizTitle).toHaveTextContent('Hva heter den magiske hunden som biter av folk øret?')
+  fireEvent.click(screen.getByText('Elton Jhon'))
+  expect(quizTitle).toHaveTextContent('Hva er Velkommen på Polsk?')
+  fireEvent.click(screen.getByText('Witaj'))
+  expect(quizTitle).toHaveTextContent('Hvorfor kan jeg ikke danse?')
+})
+it('Helper div should be hidden by default', () => {
+  render(<App />)
+  const element = document.getElementById('helper').style.display;    
+  expect(element).toBe('none');
+})
+it('renders the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+it('should render at least 4 buttons', () => {
+  render(<App />)
+  const element = document.querySelectorAll('button');
+  // expect(element).toBeInTheDocument();
+  expect(element).toHaveLength(4);
+});
+
+
